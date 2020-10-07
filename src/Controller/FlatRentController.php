be59d274653856b2c59b557d64c0078bf3e7ffec
kszahun <kszahun@arto-soft.com.pl>
2020-10-07 15:28:38 +0200

    !refactor move flat build from controler to builder

--- src/Controller/FlatRentController.php
+++ src/Controller/FlatRentController.php
@@ -4,36 +4,30 @@
 namespace App\Controller;
 
 
-use App\Entity\Flat;
+use App\Builder\FlatBuilder;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class FlatRentController extends AbstractController
 {
+    /**
+     * @var FlatBuilder
+     */
+    private $flatBuilder;
+
+    public function __construct(FlatBuilder $flatBuilder)
+    {
+        $this->flatBuilder = $flatBuilder;
+    }
 
     /**
      * @Route("/")
      */
     public function flatList()
     {
-        $flats = $this->generateNumberOfFlats(4);
+        $flats = $this->flatBuilder->buildMultipleFlats(5);
         return $this->render('flatRent/index.html.twig', [
             "flats" => $flats
         ]);
     }
-
-    /**
-     * @param int $number
-     * @return array
-     */
-    private function generateNumberOfFlats(int $number)
-    {
-        $result = [];
-        for ($i = 1; $i <= $number; $i++) {
-            $result[] = new Flat('name', 9.8, 'adres', 12);
-        }
-
-        return $result;
-    }
-
 }

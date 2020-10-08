7e8e08c5706d45a89c5d3b5a1701cdf7d99486c5
kszahun <kszahun@arto-soft.com.pl>
2020-10-09 00:40:55 +0200

    add validation for reservations

--- src/Service/ReservationFormValidator.php
+++ src/Service/ReservationFormValidator.php
@@ -5,6 +5,7 @@ namespace App\Service;
 
 
 use App\Entity\Flat;
+use App\Entity\Reservation;
 
 class ReservationFormValidator
 {
@@ -13,14 +14,35 @@ class ReservationFormValidator
      * @param int $numberOfResidents
      * @param \DateTime $from
      * @param \DateTime $to
-     * @return bool
+     * @return array
      */
     public function isValid(Flat $flat, int $numberOfResidents, \DateTime $from, \DateTime $to)
     {
         $now = new \DateTime("now");
+        if($from < $now || $to < $now) return ['isValid' => false, 'errorMsg' => "Podano datę z przeszłośći"];
+        if($from > $to) return ['isValid' => false, 'errorMsg' => "Niezgody zakres dat(od>do)"];
+        if($flat->getMaxNumberOfResidents() < $numberOfResidents) return ['isValid' => false, 'errorMsg' => "W tym mieszkaniu nie ma tylu miejsc"];
+        if($this->isDatesAlreadyReserved($flat, $from, $to)) return ['isValid' => false, 'errorMsg' => "Wybrany termin jest juz zarezerwowany"];
 
+        return ['isValid' => true];
+    }
 
-        return true;
+    private function isDatesAlreadyReserved(Flat $flat, \DateTime $from, \DateTime $to)
+    {
+        $flatReservations = $flat->getReservations();
+        if($flatReservations->count() == 0) return false;
+
+        /** @var Reservation $reservation */
+        foreach ($flatReservations as $reservation) {
+            $reservationStartDate = $reservation->getStartDate();
+            $reservationEndDate = $reservation->getEndDate();
+
+            if($reservationEndDate >= $from && $reservationStartDate < $from) return true;
+            if($reservationStartDate <= $to && $reservationEndDate > $to) return true;
+            if($reservationStartDate <= $from && $reservationEndDate >= $to) return true;
+        }
+
+        return false;
     }
 
 }
